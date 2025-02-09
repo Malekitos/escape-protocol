9c81cbb958fba765bdc1a7d4aafe83a43d0fa0c1
Dzonatans Svilis-Sudints <Dzonatan2017@gmail.com>
2025-02-09 22:10:29 +0200

    Menu

--- Scripts/proc_gen_world.gd
+++ Scripts/proc_gen_world.gd
@@ -4,7 +4,6 @@ extends Node2D
 @export var noise_tree_text : NoiseTexture2D
 
 
-@onready var tile_map = $TileMap
 var noise
 var tree_noise
 
@@ -22,27 +21,38 @@ var terrain_sand_int = 0
 var terrain_grass_int = 1
 var terrain_cliff_int = 2
 
-var water_layer = 0
-var ground_layer = 1
-var ground2_layer = 2
-var enviroment_layer = 3
-var cliff_layer = 4
+@onready var water_layer: TileMapLayer = $water_layer
+@onready var ground_layer: TileMapLayer = $ground_layer
+@onready var ground_2_layer: TileMapLayer = $ground2_layer
+@onready var enviroment_layer: TileMapLayer = $enviroment_layer
+@onready var cliffs_layer: TileMapLayer = $cliffs
+
+@onready var player: Node2D = $Player
+
+
 
 var grass_atlas_arr = [Vector2i(1,0),Vector2i(2,0),Vector2i(3,0),Vector2i(4,0)]
 
 var source_id = 0
 
-var width = 300
-var height = 300
+var width = 200
+var height = 200
 
 func _ready() -> void:
 	noise = noise_height_text.noise
+	noise.seed = randi()
 	tree_noise = noise_tree_text.noise
+
+
 
 	generate_world()
 
 
+
 func generate_world():
+	
+	var player_spawned = false
+	
 	for x in range(width):
 		for y in range(height):
 			var noise_val = noise.get_noise_2d(x,y)
@@ -51,24 +61,28 @@ func generate_world():
 			#noise_val_arr.append(noise_val)
 			
 			if noise_val > 0.0:
-				if noise_val > 0.2:
+				sand_tiles_arr.append(Vector2i(x,y))
+				if noise_val > 0.15:
 					grass_tiles_arr.append(Vector2i(x,y))
 					
 					if noise_val > 0.3:
-						tile_map.set_cell(ground2_layer, Vector2i(x,y), source_id, grass_atlas_arr.pick_random())
+						ground_2_layer.set_cell(Vector2i(x,y), source_id, grass_atlas_arr.pick_random())
 					
-					if noise_val > 0.7:
+					if noise_val > 0.50:
 						cliff_tiles_arr.append(Vector2i(x,y))
 
-				sand_tiles_arr.append(Vector2i(x,y))
-				if tree_noise_val >0.85 and noise_val > 0.2:
-					tile_map.set_cell(enviroment_layer, Vector2i(x,y),source_id, tree_atlas)
 				
-			tile_map.set_cell(water_layer, Vector2i(x,y),source_id, water_atlas)
+				if tree_noise_val >0.85 and noise_val > 0.2:
+					enviroment_layer.set_cell(Vector2i(x,y),source_id, tree_atlas)
+					
+
+			
+			elif noise_val <= 0.0:
+				water_layer.set_cell(Vector2i(x,y),source_id, water_atlas)
 	
-	tile_map.set_cells_terrain_connect(ground_layer, sand_tiles_arr, terrain_sand_int, 0)
-	tile_map.set_cells_terrain_connect(ground_layer, grass_tiles_arr, terrain_grass_int, 0)
-	tile_map.set_cells_terrain_connect(cliff_layer, cliff_tiles_arr, terrain_cliff_int, 0)
+	ground_layer.set_cells_terrain_connect(sand_tiles_arr, terrain_sand_int, 0)
+	ground_layer.set_cells_terrain_connect(grass_tiles_arr, terrain_grass_int, 0)
+	cliffs_layer.set_cells_terrain_connect(cliff_tiles_arr, terrain_cliff_int, 0)
 
 
 
