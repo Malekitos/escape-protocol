856a3d0ebf6c9abcd13bd16dac913c5bc47363bf
Dzonatans Svilis-Sudints <Dzonatan2017@gmail.com>
2025-04-08 15:08:35 +0300

    enemy AI 0.2

--- Resources/Enemys/enemy.gd
+++ Resources/Enemys/enemy.gd
@@ -2,8 +2,9 @@ extends Node2D
 
 @export var stats : enemy_stats
 @onready var enemy: Node2D = $"."
-@onready var animated_sprite_2d: AnimatedSprite2D = $CharacterBody2D/AnimatedSprite2D
+@onready var _animated_sprite: AnimatedSprite2D = $CharacterBody2D/AnimatedSprite2D
 
+@onready var character_body_2d: CharacterBody2D = $CharacterBody2D
 
 
 var enemy_name : String
@@ -13,10 +14,58 @@ var speed : int
 var player : Node2D 
 	
 func _ready() -> void:
-	animated_sprite_2d.sprite_frames = stats.texture
+	_animated_sprite.sprite_frames = stats.texture
 	enemy.modulate = stats.modulate
-	animated_sprite_2d.play("idle")
+	_animated_sprite.play("idle")
 	enemy.name = stats.name
 	enemy.health = stats.health
 	enemy.speed = stats.speed
 	player = get_tree().get_first_node_in_group('player')
+
+	
+
+func damage(attack: Attack):
+	health -= attack.attack_damage
+	var delay_timer = get_tree().create_timer(0.3)
+	
+	enemy.modulate = Color(100,100,100,100)
+	character_body_2d.speed = 0
+	
+	var dir = character_body_2d.dir
+	
+	if abs(dir.x) > abs(dir.y):
+		if dir.x > 0:
+			_animated_sprite.play("hurt_right")
+			_animated_sprite.flip_h = false
+		else:
+			_animated_sprite.play("hurt_right")
+			_animated_sprite.flip_h = true
+	else:
+		if dir.y > 0:
+			_animated_sprite.play("hurt") 
+		else:
+			_animated_sprite.play("hurt")  
+	
+
+		
+	await delay_timer.timeout
+	$CharacterBody2D.speed = stats.speed
+	enemy.modulate = stats.modulate
+	
+	if health < 0:
+
+		#inventory.add_item(preload("res://Resources/items/materials/wood.tres"))
+		_animated_sprite.play("death_front")
+		
+		queue_free()
+		return
+		
+
+
+
+func _on_damages_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.is_pressed():
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			var attack = Attack.new()
+			attack.attack_damage = player.attack_damage
+			damage(attack)
--- Resources/Enemys/enemy_AI.gd
+++ Resources/Enemys/enemy_AI.gd
@@ -8,14 +8,15 @@ extends CharacterBody2D
 
 @onready var player = get_tree().get_first_node_in_group('player')
 
+var dir : Vector2
+
 	
 func _physics_process(delta: float) -> void:
-	var dir = (navigation_agent_2d.get_next_path_position() - global_position).normalized()
+	dir = (navigation_agent_2d.get_next_path_position() - global_position).normalized()
 	velocity = dir * speed
 	move_and_slide()
 	
 	if abs(dir.x) > abs(dir.y):
-		# Горизонтальное движение
 		if dir.x > 0:
 			_animated_sprite.play("walk_right")
 			_animated_sprite.flip_h = false
@@ -23,11 +24,11 @@ func _physics_process(delta: float) -> void:
 			_animated_sprite.play("walk_right")
 			_animated_sprite.flip_h = true
 	else:
-		# Вертикальное движение
+
 		if dir.y > 0:
-			_animated_sprite.play("walk")  # или walk_forward
+			_animated_sprite.play("walk")  
 		else:
-			_animated_sprite.play("walk_back")    # или walk_back
+			_animated_sprite.play("walk_back")    
 
 func makepath() -> void:
 	navigation_agent_2d.target_position = player.global_position
@@ -35,3 +36,4 @@ func makepath() -> void:
 
 func _on_timer_timeout() -> void:
 	makepath()
+	
