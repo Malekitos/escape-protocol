552159672b2387d7d3c25f103635033f376ecd1d
Dzonatans Svilis-Sudints <Dzonatan2017@gmail.com>
2025-04-08 14:37:55 +0300

    enemy animation

--- Resources/Enemys/enemy_AI.gd
+++ Resources/Enemys/enemy_AI.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
+@onready var _animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 @onready var enemy_stats = get_parent().stats
 @onready var speed = enemy_stats.speed
@@ -12,6 +13,21 @@ func _physics_process(delta: float) -> void:
 	var dir = (navigation_agent_2d.get_next_path_position() - global_position).normalized()
 	velocity = dir * speed
 	move_and_slide()
+	
+	if abs(dir.x) > abs(dir.y):
+		# Горизонтальное движение
+		if dir.x > 0:
+			_animated_sprite.play("walk_right")
+			_animated_sprite.flip_h = false
+		else:
+			_animated_sprite.play("walk_right")
+			_animated_sprite.flip_h = true
+	else:
+		# Вертикальное движение
+		if dir.y > 0:
+			_animated_sprite.play("walk")  # или walk_forward
+		else:
+			_animated_sprite.play("walk_back")    # или walk_back
 
 func makepath() -> void:
 	navigation_agent_2d.target_position = player.global_position
